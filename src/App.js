73e1b37f0189916078c96bd472f5e747121b1440
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-13 00:55:59 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Navbar';
 function App() {
   const title = "Welcome to the new blog"
   return (
     <div className="App">
+        <Navbar/>
       <div className='content'>
         <h1>{title}</h1>
       </div>
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+        <nav className="navbar">
+            <h1>Welcome To Nacer Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create"> new blog</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
