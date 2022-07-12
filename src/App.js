63135660272e3298590151244b543e54d656c4dc
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-13 00:23:31 +0100

    Create a variable

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog"
   return (
     <div className="App">
       <div className='content'>
-        <h1>This a component</h1>
+        <h1>{title}</h1>
       </div>
     </div>
   );
