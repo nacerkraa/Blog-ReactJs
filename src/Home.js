91f24d15991456e1a65304ee846c8c3f32649512
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-13 20:40:43 +0100

    Handle the Event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
 const Home = () => {
+    const name = "Mario";
+    const handleClick = () => {
+        console.log('Hello Ningas');
+    }
+
+    const handleClickAgain = (name,e) => {
+        console.log('Hello ' + name , e.target);
+    }
+
     return(
+        
         <div className="Home">
-            <h1>Welcome To Home Page</h1>
+            <p>{name}</p>
+            <h1>Welcome To Home Page Content</h1>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('nacer',e) }>Click me again</button>
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,16 @@
 const Navbar = () => {
     return(
         <nav className="navbar">
-            <h1>Welcome To Nacer Blog</h1>
+            <h1>Brand</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create"> new blog</a>
+                <a href="/">About</a>
+                <a href="/">Login</a>
+                <a href="/create" style={{
+                    color: "white",
+                    backgroundColor:'#f1356d',
+                    borderRadius:'5px'
+                }}> Register</a>
             </div>
         </nav>
     );
