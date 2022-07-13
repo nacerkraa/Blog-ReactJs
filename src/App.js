aaa69bcab2008639d440d8a056b271d670743a85
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-13 15:09:27 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
 function App() {
-  const title = "Welcome to the new blog"
   return (
     <div className="App">
         <Navbar/>
       <div className='content'>
-        <h1>{title}</h1>
         <Home/>
       </div>
     </div>
