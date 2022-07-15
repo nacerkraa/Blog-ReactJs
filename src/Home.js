d7412c65d92b79b9def55d797cd0f6cd85d64c83
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-15 16:41:32 +0100

    filter item on the list of items on react

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,15 @@ const Home = () => {
     const [blogs,setBlogs] = useState([
         {title:"Html, and css", body:"lorem ipsum...", auther:"nacer", id:1},
         {title:"How to learn react easly", body:"lorem ipsum...", auther:"kaido", id:2},
-        {title:"The best programming languiges", body:"lorem ipsum...", auther:"mikasa", id:3}
+        {title:"Html, and css and js", body:"lorem ipsum...", auther:"nacer", id:3},
+        {title:"The best programming languiges", body:"lorem ipsum...", auther:"mikasa", id:4}
     ]);
 
     return(
         
         <div className="Home">
             <BlogList blogs={blogs} title="The list of the blogs"/>
+            <BlogList blogs={blogs.filter((blog) => blog.auther === 'nacer')} title="Nacer's Blogs"/>
         </div>
     );
 }
