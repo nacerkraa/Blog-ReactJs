48536a8b6b0e503ee7d09441874735bbf2fc14c1
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-15 18:04:56 +0100

    Using Effects

--- src/BlogList.js
+++ src/BlogList.js
@@ -7,7 +7,7 @@ const BlogList = ({ blogs, title, handleDelete }) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
                     <p>{blog.body}</p>
-                    <button onClick={() => handleDelete(blog.id)}>{blog.auther}</button>
+                    <button onClick={() => handleDelete(blog.id)}>Delte blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
     const [blogs,setBlogs] = useState([
@@ -12,6 +12,10 @@ const Home = () => {
         const newBlog = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlog);
     }
+
+    useEffect(() => {
+        console.log("Use Effect Run");
+    });
 
     return(
         
