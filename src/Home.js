858642d1d36f6f4e47b613b28a9250c1d9030f2a
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-15 12:07:21 +0100

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
 const Home = () => {
-    const name = "Mario";
+    let name = "Mario";
     const handleClick = () => {
-        console.log('Hello Ningas');
+        name = "Yushi";
+        console.log(name);
     }
 
-    const handleClickAgain = (name,e) => {
-        console.log('Hello ' + name , e.target);
-    }
 
     return(
         
@@ -15,7 +13,6 @@ const Home = () => {
             <p>{name}</p>
             <h1>Welcome To Home Page Content</h1>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('nacer',e) }>Click me again</button>
         </div>
     );
 }
