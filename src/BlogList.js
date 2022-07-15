642e34031a3859bd59dfc467d9b55179e143b2b8
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-15 16:33:00 +0100

    Update props method

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,6 @@
-const BlogList = (props) => {
-    const blogs = props.blogs;
-    const title = props.title;
+const BlogList = ({blogs,title}) => {
+   // const blogs = props.blogs;
+   // const title = props.title;
     return ( 
         <div className="blog-list">
             <h2>{title}</h2>
