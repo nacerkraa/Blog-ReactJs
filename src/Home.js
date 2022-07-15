010c523414a8a5b7726bd63f65cec31ef2147b33
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-15 18:56:11 +0100

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
+    
     const [blogs,setBlogs] = useState([
         {title:"Html, and css", body:"lorem ipsum...", auther:"nacer", id:1},
         {title:"How to learn react easly", body:"lorem ipsum...", auther:"kaido", id:2},
