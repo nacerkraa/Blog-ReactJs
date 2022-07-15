ebf44dc629265393ffea4c13743d2d60fbb4c99b
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-15 13:33:22 +0100

    Add age attribute

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,18 @@ import { useState } from "react";
 const Home = () => {
     // let name = "Mario";
     const [name,setName] = useState("Mario");
+    const [age,setAge] = useState(23);
     
     const handleClick = () => {
         setName("Kaido");
+        setAge(25);
     }
 
 
     return(
         
         <div className="Home">
-            <p>{name}</p>
+            <p>{name} is {age} years old</p>
             <h1>Welcome To Home Page Content</h1>
             <button onClick={handleClick}>Click me</button>
         </div>
