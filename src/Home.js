1a0ab2a8961f67d4d96099ed89d6832fdaca4e48
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-18 17:46:03 +0100

    Peste the code into the useFetch file

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,6 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    const handleDelete = (id) => {
-        const newBlog = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlog);
-    }
-
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if (!res.ok) {
-                    throw Error("Could not fetch the data for that resorce...")
-                }
-                return res.json();
-            })
-
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-            })
-    }, []);
-
     
     return (
 
---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useFetch = () => {
+    useEffect(() => {
+        const [data, setData] = useState(null);
+        const [isPending, setIsPending] = useState(true);
+        const [error, setError] = useState(null);
+    
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if (!res.ok) {
+                    throw Error("Could not fetch the data for that resorce...")
+                }
+                return res.json();
+            })
+
+            .then(data => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
+    }, []);
+}
+
+export default useFetch;
