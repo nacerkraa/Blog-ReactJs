b8a4f60283ee691f9767a7a6c052d8bbd5f325e7
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-18 22:35:32 +0100

    Update

--- src/useFetch.js
+++ src/useFetch.js
@@ -24,7 +24,7 @@ const useFetch = (url) => {
                 setIsPending(false);
                 setError(err.message);
             })
-    }, []);
+    }, [url]);
 
     return {data, isPending, error}
 }
