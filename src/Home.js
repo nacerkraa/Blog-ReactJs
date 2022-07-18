47bc12c7c8f06cf9c3646f39d492f9ce7148ae5f
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-19 00:01:23 +0100

    Update react router dom

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const Home = () => {
 
     const {data:blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
