9b789bac36bdc22c93d7b2db9ffb668504dc6e2e
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-18 16:50:59 +0100

    Update

--- src/Home.js
+++ src/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
                 if (!res.ok) {
-                    throw Error("Could not fetch the data for that resorce")
+                    throw Error("Could not fetch the data for that resorce...")
                 }
                 return res.json();
             })
