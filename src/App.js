ff9c3ae345c950b22719afa0e1c8f37004cf6c0b
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-19 00:09:12 +0100

    Create Home Route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
+    <Router>
+      <div className="App">
+        <Navbar/>
 
-      <div className='content'>
-        <Home/>
+        <div className='content'>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+        
       </div>
-      
-    </div>
+    </Router>
   );
 }
 
