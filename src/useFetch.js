0f30cd7ed5bd2ac0f5191a61431cb6ff30b1ba2f
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-19 21:48:00 +0100

    add return func

--- src/useFetch.js
+++ src/useFetch.js
@@ -27,7 +27,7 @@ const useFetch = (url) => {
                 setError(err.message);
             })
         }, 1000);
-        
+        return () => console.log("Cleanup")
     }, [url]);
 
     return {data, isPending, error}
