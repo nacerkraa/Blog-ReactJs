6867be261383b74bf0974b3ee37e4a85b2b4b061
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-20 23:06:05 +0100

    Add detail page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
+    const {data:blog, error, isPending} = useFetch("http://localhost:8000/blogs/" + id);
     return ( 
-        <h2>Blogs Details -{id}</h2>
-     );
+        <div className="blog-details">
+            {isPending && <div>Loading ...</div>}
+            {error && <div>{error}</div>}
+            {blog && 
+            <article>
+                <h2>{blog.title}</h2>
+                <p>writer by {blog.auther}</p>
+                <div>{blog.body}</div>
+            </article>}
+        </div>
+    );
 }
  
 export default BlogDetails;
