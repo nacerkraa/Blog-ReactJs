20f9fe7817e9515ece3365cd0a7ae41ef3a2a44e
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-21 18:37:45 +0100

    Update

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,22 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <label>Blog title:</label>
+            <input 
+                type="text"
+                required
+            />
+
+            <label>Blog body:</label>
+            <textarea 
+                required
+            ></textarea>
+
+            <label>Auther:</label>
+            <select>
+                <option value="mario">mario</option>
+                <option value="yoshi">yoshi</option>
+            </select>
         </div>
      );
 }
