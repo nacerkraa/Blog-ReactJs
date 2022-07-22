d67a30a6397f467e5b405dd51f759d55fc6db150
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-22 22:31:28 +0100

    Update

--- src/Create.js
+++ src/Create.js
@@ -6,7 +6,7 @@ const Create = () => {
 
     return (
         <div className="create">
-            <h2>Add a New Blog</h2>
+            <h2>Add a New Blog</h2> 
             <from>
             <label>Blog title:</label>
             <input 
