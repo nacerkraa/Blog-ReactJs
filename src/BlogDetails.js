6f3a85d732936433e3a2a1ce26ef549996608020
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-25 14:48:59 +0100

    fix error

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -7,7 +7,7 @@ const BlogDetails = () => {
     const history = useHistory();
 
     const handleDelete = () => {
-        fetch('http://localhost:8000/blogs' + blog.id, {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
             method : 'DELETE'
         }).then(() => {
             history.push('/');
